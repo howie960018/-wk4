f5f32c1d663142e68f529f7b44ec506b9dc1b06a
howie960018 <84056899+howie960018@users.noreply.github.com>
2021-10-12 11:13:28 +0800

    Add files via upload

---
+++ 1012-1.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+Servo myservo;
+const int servoPin=14;
+int pos=0;
+
+void setup() {
+  myservo.attach(servoPin);
+  Serial.begin(115200);
+  pinMode(13,INPUT);
+}
+
+void loop() {
+
+  int input=analogRead(13);
+  pos=map(input,0,4095,0,180);
+  myservo.write(pos);
+  Serial.println(pos);
+  delay(50);
+ 
+}
